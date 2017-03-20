68dca9b91867f6123739e1c5dd7af3a1ffa6b2a0
John Paraskevopoulos <io.paraskev@gmail.com>
2017-03-20 16:40:03 +0200

    documentation and versioning fixes

--- setup.py
+++ setup.py
@@ -48,12 +48,11 @@ setup(
     zip_safe=False,
     keywords='onionrouter',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
